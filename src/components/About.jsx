89a0097bd28bcfdea4a1c0caed534e365a40ea3f
UmarAbdullah79 <uu0339843@gmail.com>
2025-10-22 23:43:49 +0530

    skill section varaikkum added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -33,27 +33,25 @@ function About() {
     //     </div>
     //   </div>
     // </div>
-    <div className="">
-      <div className="px-5">
-        <div className="bg-gradient-to-br from-[#210b2c] to-[#5c2a72] w-full rounded-2xl p-2 lg:p-10">
-          <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8">
-            <div className=" p-5 lg:pb-20 rounded-xl text-white flex justify-center items-end">
-              <p>
-                Passionate about turning ideas into impactful digital
-                experiences.
-              </p>
-            </div>
+    <div className="mt-10">
+      <div className="">
+        {/* <div className="bg-gradient-to-br from-[#210b2c] to-[#5c2a72] w-full rounded-2xl p-2 lg:p-10"> */}
+        <div className="w-full p-2 lg:p-10 bg-[url('/images/waves-2.svg')] bg-bottom bg-cover">
+          <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8">
+            {/* <div className=" p-5 lg:pb-20 rounded-xl text-[#210b2c] flex justify-center items-end">
+              <p>Passionate about turning ideas into impactful digital experiences.</p>
+            </div> */}
 
-            <div className="p-5 rounded-xl lg:col-span-2 text-white">
+            <div className="p-5 rounded-xl lg:col-span-2 text-[#210b2c]">
               <h2 className="text-5xl font-semibold">
                 Let's know more about me
               </h2>
               <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8 mt-10">
-                <div className="p-5 pl-0 rounded-xl text-white">
+                <div className="p-5 pl-0 rounded-xl text-[#210b2c]">
                   <p className="text-lg">This is me</p>
                 </div>
 
-                <div className="p-5 pl-0 rounded-xl lg:col-span-2 text-white">
+                <div className="p-5 pl-0 rounded-xl lg:col-span-2 text-gray-900">
                   <p className="">
                     I'm a{" "}
                     <span className="font-semibold text-xl">
@@ -72,14 +70,14 @@ function About() {
                   </p>
                 </div>
               </div>
-              <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8 mt-10">
+              <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 lg:gap-8 mt-10">
                 <div className=" rounded space-y-5 mb-10">
-                  <h2 className="text-gray-400">(01)</h2>
-                  <hr className="w-[80%] mt-2 text-white/40" />
-                  <h2 className="text-white text-2xl">
+                  <h2 className="text-gray-900">(01)</h2>
+                  <hr className="w-[80%] mt-2 text-black" />
+                  <h2 className="text-[#210b2c] text-2xl">
                     Professional Experience
                   </h2>
-                  <p className="text-gray-300">
+                  <p className="text-gray-900">
                     Boosting business growth through strategic marketing, client
                     engagement, and performance-driven campaigns - enhancing
                     brand visibility and lasting relationships.
@@ -87,10 +85,12 @@ function About() {
                 </div>
 
                 <div className=" rounded space-y-5 mb-10">
-                  <h2 className="text-gray-400">(02)</h2>
-                  <hr className="w-[80%] mt-2 text-white/40" />
-                  <h2 className="text-white text-2xl">Software Development</h2>
-                  <p className="text-gray-300">
+                  <h2 className="text-gray-900">(02)</h2>
+                  <hr className="w-[80%] mt-2 text-black" />
+                  <h2 className="text-[#210b2c] text-2xl">
+                    Software Development
+                  </h2>
+                  <p className="text-gray-900">
                     Building scalable web applications with Angular and Node.js,
                     delivering responsive UIs, seamless APIs, and efficient
                     backend integrations through collaborative development and
@@ -99,10 +99,10 @@ function About() {
                 </div>
 
                 <div className=" rounded space-y-5 mb-10">
-                  <h2 className="text-gray-400">(03)</h2>
-                  <hr className="w-[80%] mt-2 text-white/40" />
-                  <h2 className="text-white text-2xl">Freelance works</h2>
-                  <p className="text-gray-300">
+                  <h2 className="text-gray-900">(03)</h2>
+                  <hr className="w-[80%] mt-2 text-black" />
+                  <h2 className="text-[#210b2c] text-2xl">Freelance works</h2>
+                  <p className="text-gray-900">
                     Alongside my career, I freelance as a branding and
                     advertising consultant, helping businesses craft impactful
                     digital identities that connect with audiences and drive
@@ -111,10 +111,10 @@ function About() {
                 </div>
 
                 <div className=" rounded space-y-5 mb-10">
-                  <h2 className="text-gray-400">(04)</h2>
-                  <hr className="w-[80%] mt-2 text-white/40" />
-                  <h2 className="text-white text-2xl">My key focus</h2>
-                  <p className="text-gray-300">
+                  <h2 className="text-gray-900">(04)</h2>
+                  <hr className="w-[80%] mt-2 text-black" />
+                  <h2 className="text-[#210b2c] text-2xl">My key focus</h2>
+                  <p className="text-gray-900">
                     Specializing in personal branding, creative direction, and
                     social media growth — helping professionals build a strong
                     online identity through impactful design, targeted ads, and
@@ -125,6 +125,9 @@ function About() {
             </div>
           </div>
         </div>
+        {/* <div>
+          <img src="/images/waves.svg" alt="" className="w-full rounded-2xl" />
+        </div> */}
       </div>
     </div>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,19 @@
 function Hero() {
   return (
     <>
-      <div className="bg-gradient-to-br from-[#210b2c] to-[#5c2a72] h-[100vh] grid grid-cols-1 md:grid-cols-2 text-white [clip-path:polygon(0_0,100%_0,100%_90%,50%_100%,0_90%)]">
+      <div className="bg-gradient-to-br from-[#210b2c] to-[#5c2a72] h-[100vh] grid grid-cols-1 md:grid-cols-2 text-white">
         {/* content-side */}
         <div className="pt-[8vh] pl-10 flex flex-col justify-center gap-10">
-          <h2 className="pl-4 text-4xl font-bold border-l-4 border-white text-g">
+          <h2 className="pl-4 lg:text-4xl md:text-3xl text-2xl font-bold border-l-4 border-white text-g">
             I’m Rakesh Mahendren
           </h2>
-          <p className="text-xl">
-            Engineer by profession, entrepreneur by vision, and exporter by
-            passion, I specialize in driving innovation, business growth, and
-            international trade. With expertise in engineering solutions, global
-            market expansion, and export operations, I help businesses scale and
-            connect with global opportunities
+          <p className="md:text-xl text-sm md:inline-block">
+            <span className="tg font-bold">Engineer</span> by profession,
+            <span className="tg font-bold"> Entrepreneur</span> by vision, and
+            <span className="tg font-bold"> Exporter</span> by passion — I drive
+            innovation, business growth, and global trade. With expertise in
+            engineering solutions and international markets, I help businesses
+            expand and connect worldwide.
           </p>
         </div>
         {/* image side */}
@@ -20,22 +21,10 @@ function Hero() {
           <img
             src="/images/heropic.png"
             alt="HERO"
-            className="h-[90vh] relative z-10"
+            className="lg:h-[90vh] pt-15 md:pt-0 md:h-[80vh] h-50vh relative z-10"
           />
-          <div className="bg-[#BC96E6] absolute top-[40%] z-2 opacity-40 h-[400px] w-[400px] rounded-full"></div>
+          <div className="bg-[#BC96E6] hidden lg:block absolute top-[40%] z-2 opacity-40 h-[450px] w-[450px] rounded-full"></div>
         </div>
-        {/* <svg
-          class="absolute bottom-0 left-0 w-full"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1440 320"
-        >
-          <path
-            fill="white"
-            d="M0,224 C480,320 960,128 1440,224 L1440,320 L0,320 Z
-M0,224 C480,320 960,128 1440,224 L1440,320 L0,320 Z
-"
-          ></path>
-        </svg> */}
       </div>
     </>
   );
